c7ea197dc0147066a3c2855341045b0b810ad478
guidanoli <guidanoli@hotmail.com>
2019-11-16 13:33:03 -0300

    Finished + README Running section

--- README.md
+++ README.md
@@ -5,3 +5,30 @@ Brainfuck is an esoteric language. A strangely simple esoteric language. That me
 Brainfuck works much like a Turing Machine: a (nearly infinite) tape, a head, and a state register. Plus input and output operators and loops. To read more about brainfuck, check out its article on Esolang [here](https://esolangs.org/wiki/Brainfuck)!
 
 I chose Lua mainly because it handles arbitrary precision numbers, and hash tables natively, which really helps the compiler scalability. Plus Lua is really lightweight, unlike Python.
+
+## Running
+
+Simply run the lua script on your terminal...
+
+```bash
+lua bf.lua
+```
+
+...and type/paste the brainfuck code and then escape input (Ctrl+D on Unix, Ctrl+Z on Windows).
+...if requested input, type and escape ever after each character. Cease input by escaping twice.
+
+Here is a demo of the interpreter running [this script](http://www.hevanet.com/cristofd/brainfuck/wc.b) by [daniel b cristofani](http://www.hevanet.com/cristofd/brainfuck/), which counts lines, words and bytes of input:
+
+```
+>>>+>>>>>+>>+>>+[<<],[
+    -[-[-[-[-[-[-[-[<+>-[>+<-[>-<-[-[-[<++[<++++++>-]<
+        [>>[-<]<[>]<-]>>[<+>-[<->[-]]]]]]]]]]]]]]]]
+    <[-<<[-]+>]<<[>>>>>>+<<<<<<-]>[>]>>>>>>>+>[
+        <+[
+            >+++++++++<-[>-<-]++>[<+++++++>-[<->-]+[+>>>>>>]]
+            <[>+<-]>[>>>>>++>[-]]+<
+        ]>[-<<<<<<]>>>>
+    ],
+]+<++>>>[[+++++>>>>>>]<+>+[[<++++++++>-]<.<<<<<]>>>>>>>>]
+i would like a cup of tea	0	7	25
+```
--- bf.lua
+++ bf.lua
@@ -1,33 +1,17 @@
 -- Brainfuck Interpreter
 
--- For debugging purposes
-function dump_ (t, sep)
-    for k, v in pairs(t) do
-        if type(v) == "table" then
-            print(sep.."["..tostring(k).."] = {")
-            dump_(v, sep .. "\t")
-            print(sep.."}")
-        else
-            print(sep.."["..tostring(k).."] = "..tostring(v))
-        end
-    end
-end
-
-function dump (t)
-    print("{")
-    dump_(t, "\t")
-    print("}")
-end
-------------------------------
-
 local code = io.read("*all"):gsub("[^><+-.,%[%]]", "")
 
 local bracket_lvl = 0
 for c in code:gmatch("[%[%]]") do
     bracket_lvl = bracket_lvl + (c == "[" and 1 or -1)
+    if bracket_lvl < 0 then
+        io.stderr:write("Wrong bracketing!\n")
+        os.exit()
+    end
 end
 if bracket_lvl ~= 0 then
-    print("Wrong bracketing!")
+    io.stderr:write("Wrong bracketing!\n")
     os.exit()
 end
 
@@ -64,7 +48,6 @@ end
 function build_code_table (proc_id)
     local t = procedures[proc_id]
     for i, v in ipairs(t) do
-        print(i, v, type(v))
         if type(v) == "number" then
             t[i] = build_code_table(v)
         end
@@ -72,6 +55,41 @@ function build_code_table (proc_id)
     return t
 end
 
-code_table = build_code_table(0)
+local code_table = build_code_table(0)
+local tape_table = { [0] = 0 }
+local head_loc = 0
 
-dump(code_table)
+function run_code (proc_s)
+    for c in proc_s:gmatch(".") do
+        if c == "+" then
+            tape_table[head_loc] = tape_table[head_loc] + 1
+        elseif c == "-" then
+            tape_table[head_loc] = tape_table[head_loc] - 1
+        elseif c == "<" then
+            head_loc = head_loc - 1
+        elseif c == ">" then
+            head_loc = head_loc + 1
+        elseif c == "." then
+            io.write(string.char(tape_table[head_loc]))
+        elseif c == "," then
+            local ch = io.read(1)
+            tape_table[head_loc] = ch and ch:byte() or 0
+        end
+        tape_table[head_loc] = tape_table[head_loc] and
+            (tape_table[head_loc] % (2^32)) or 0
+    end
+end
+
+function run_procedure (proc_t)
+    for _, subproc in ipairs(proc_t) do
+        if type(subproc) == "table" then
+            while tape_table[head_loc] ~= 0 do
+                run_procedure(subproc)
+            end
+        else
+            run_code(subproc)
+        end
+    end
+end
+
+run_procedure(code_table)
